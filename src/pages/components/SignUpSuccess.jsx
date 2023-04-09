896ae7a1cb1dbc1420cf83fc17767d521c405b8d
0uizi0 <0uizi0@naver.com>
2023-04-10 00:17:07 +0900

    회원가입 완료 컴포넌트 영역 생성

---
+++ src/pages/components/SignUpSuccess.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import logoImg from '../../assets/images/github-icon.png';
+import styled from 'styled-components';
+
+const MySignUpFinish = styled.div`
+  width: 100%;
+  padding: 0;
+  text-align: center;
+  margin: 40px 0 0 0;
+`;
+
+const MyLogo = styled.img`
+  margin-top: 70px;
+  width: 200px;
+  object-fit: contain;
+`;
+
+const MyPageBtn = styled.div`
+  font-size: 14px;
+  text-align: center;
+  width: 120px;
+  height: 39px;
+  line-height: 40px;
+  background-color: ${props => props.backgroundColor};
+  color: ${props => props.color};
+  cursor: pointer;
+  margin: 30px auto;
+`;
+
+export default function SignUpSuccess() {
+  return (
+    <MySignUpFinish>
+      <MyLogo src={logoImg} alt="임시 로고 이미지" />
+      <h2>감사합니다</h2>
+      <p>회원가입이 완료되었습니다.</p>
+      <MyPageBtn backgroundColor="#3c62e5" color="#fff">
+        홈으로
+      </MyPageBtn>
+    </MySignUpFinish>
+  );
+}
