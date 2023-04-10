806cb3b75417c092bd123fd740ab285b0264c71b
0uizi0 <0uizi0@naver.com>
2023-04-10 09:47:35 +0900

    회원가입 페이지 레이아웃 생성

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import InfoArea from './components/InfoArea';
 import styled from 'styled-components';
+import SocialInfo from './components/SocialInfo';
+import DetailInfoArea from './components/DetailInfoArea';
+import SignUpSuccess from './components/SignUpSuccess';
 
 const MyStageArea = styled.div`
   width: 100%;
@@ -28,11 +31,15 @@ const MyPageBtn = styled.div`
 
 export default function SignUp() {
   return (
-    <div style={{ padding: '5%' }}>
+    <div style={{ padding: '5% 25%' }}>
       <h1>회원가입</h1>
+      <p>소셜 로그인 및 이메일로 가입할 수 있습니다.</p>
       <hr />
-      <InfoArea />
-      <MyStageArea>
+      {/* <SocialInfo /> */}
+      {/* <InfoArea /> */}
+      {/* <DetailInfoArea /> */}
+      <SignUpSuccess />
+      {/* <MyStageArea>
         <MyPageBtn backgroundColor="#333333" color="#fff">
           이전
         </MyPageBtn>
@@ -40,7 +47,7 @@ export default function SignUp() {
         <MyPageBtn backgroundColor="#3c62e5" color="#fff">
           다음 단계로
         </MyPageBtn>
-      </MyStageArea>
+      </MyStageArea> */}
     </div>
   );
 }
