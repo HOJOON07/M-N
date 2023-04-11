0a6cf1826e596873b8cdae73d7017e573c9d2ccb
0uizi0 <0uizi0@naver.com>
2023-04-11 13:45:41 +0900

    칸반보드 컴포넌트에서 진행 리스트 영역 분리 및 연결

--- src/pages/components/Kanban.jsx
+++ src/pages/components/Kanban.jsx
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import menu from '../../assets/images/menu.png';
 import defaultProfile from '../../assets/images/default-profile.png';
+import KanbanProgress from './KanbanProgress';
 
 // Color Variables
 const backColor = '#eef1f5';
-const contentColor = '#fff';
-const subColor = '#cbcbcb';
 
 // Styled Components
 const MyTitle = styled.p`
@@ -18,32 +17,6 @@ const MyTitle = styled.p`
 const MySubTitle = styled.p`
   font-size: ${props => props.fontSize};
   text-decoration-line: underline;
-`;
-
-const MyMenuBar = styled.img`
-  width: 15px;
-`;
-
-const MyImportanceButton = styled.button`
-  padding: 5px 10px;
-  box-sizing: border-box;
-  font-size: 10px;
-  font-weight: 700;
-  border-radius: 10px;
-  border: none;
-  background-color: ${props =>
-    props.importance === 'high'
-      ? '#EB5851'
-      : props.importance === 'medium'
-      ? '#F7CF4C'
-      : '#3862B1'};
-  color: white;
-  cursor: pointer;
-  transition: 0.2s;
-
-  &:hover {
-    background-color: 'black';
-  }
 `;
 
 const MyTitleArea = styled.div`
@@ -70,58 +43,12 @@ const MyProgressArea = styled.div`
   }
 `;
 
-const MyProgressTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 0;
-  margin-bottom: 15px;
-  & > div {
-    display: flex;
-    & > p {
-      margin-right: 10px;
-    }
-  }
-`;
-
-const MyTask = styled.div`
-  border: 1px solid #bcc2d1;
-  border-radius: 5px;
-  background-color: ${contentColor};
-  padding: 10px 10px 5px;
-  margin-bottom: 20px;
-  cursor: pointer;
-
-  & > div {
-    display: flex;
-    align-content: center;
-    justify-content: space-between;
-  }
-
-  & img {
-    width: 18px;
-    height: 18px;
-  }
-  & > p {
-    margin: 7px 0;
-  }
-  & span {
-    font-size: 12px;
-    margin-left: 5px;
-  }
-`;
-
-const MyContent = styled.p`
-  font-weight: 700;
-`;
-const MyCreateData = styled.p`
-  color: ${subColor};
-  font-size: 10px;
-`;
-
 export default function Kanban() {
   /** 임시로 특정 워크스페이스(id가 0) 지정 */
   const workspaceList = useSelector(state => state.workspace.workspaceList)[0];
   const workflowList = workspaceList.workflow[0].list;
+  const iconList = ['✉️', '🔨', '📌', '🔒', '🎉'];
+  // console.log(workspaceList);
   return (
     <div>
       <MyTitleArea>
@@ -129,167 +56,31 @@ export default function Kanban() {
         <MySubTitle fontSize="14px">{workspaceList.name}</MySubTitle>
       </MyTitleArea>
       <MyProgressArea>
-        <div>
-          <MyProgressTitle>
-            <div>
-              <p>✉️</p>
-              <MyTitle fontSize="16px">Request</MyTitle>
-            </div>
-            <MyMenuBar src={menu} alt="menu-bar" />
-          </MyProgressTitle>
-
-          <MyTask>
-            <MyTitle fontSize="13px">➕ Add New Task</MyTitle>
-          </MyTask>
-          {workflowList.todo.map(el => {
-            return (
-              <MyTask key={el.id}>
-                <div>
-                  <MyContent>{el.content}</MyContent>
-                  <div>
-                    <span>✏️</span>
-                    <span>❌</span>
-                  </div>
-                </div>
-                <MyCreateData>{el.createData}</MyCreateData>
-                <div>
-                  <MyImportanceButton {...el}>
-                    {el.importance}
-                  </MyImportanceButton>
-                  <img src={defaultProfile} alt="기본 프로필 이미지" />
-                </div>
-              </MyTask>
-            );
-          })}
-        </div>
-        <div>
-          <MyProgressTitle>
-            <div>
-              <p>🔨</p>
-              <MyTitle fontSize="16px"> In progress</MyTitle>
-            </div>
-            <MyMenuBar src={menu} alt="menu-bar" />
-          </MyProgressTitle>
-          <MyTask>
-            <MyTitle fontSize="13px">➕ Add New Task</MyTitle>
-          </MyTask>
-          {workflowList.inprogress.map(el => {
-            return (
-              <MyTask key={el.id}>
-                <div>
-                  <MyContent>{el.content}</MyContent>
-                  <div>
-                    <span>✏️</span>
-                    <span>❌</span>
-                  </div>
-                </div>
-                <MyCreateData>{el.createData}</MyCreateData>
-                <div>
-                  <MyImportanceButton {...el}>
-                    {el.importance}
-                  </MyImportanceButton>
-                  <img src={defaultProfile} alt="기본 프로필 이미지" />
-                </div>
-              </MyTask>
-            );
-          })}
-        </div>
-        <div>
-          <MyProgressTitle>
-            <div>
-              <p>📌</p>
-              <MyTitle fontSize="16px">In Review</MyTitle>
-            </div>
-            <MyMenuBar src={menu} alt="menu-bar" />
-          </MyProgressTitle>
-          <MyTask>
-            <MyTitle fontSize="13px">➕ Add New Task</MyTitle>
-          </MyTask>
-          {workflowList.inreview.map(el => {
-            return (
-              <MyTask key={el.id}>
-                <div>
-                  <MyContent>{el.content}</MyContent>
-                  <div>
-                    <span>✏️</span>
-                    <span>❌</span>
-                  </div>
-                </div>
-                <MyCreateData>{el.createData}</MyCreateData>
-                <div>
-                  <MyImportanceButton {...el}>
-                    {el.importance}
-                  </MyImportanceButton>
-                  <img src={defaultProfile} alt="기본 프로필 이미지" />
-                </div>
-              </MyTask>
-            );
-          })}
-        </div>
-        <div>
-          <MyProgressTitle>
-            <div>
-              <p>🔒</p>
-              <MyTitle fontSize="16px"> Blocked</MyTitle>
-            </div>
-            <MyMenuBar src={menu} alt="menu-bar" />
-          </MyProgressTitle>
-          <MyTask>
-            <MyTitle fontSize="13px">➕ Add New Task</MyTitle>
-          </MyTask>
-          {workflowList.blocked.map(el => {
-            return (
-              <MyTask key={el.id}>
-                <div>
-                  <MyContent>{el.content}</MyContent>
-                  <div>
-                    <span>✏️</span>
-                    <span>❌</span>
-                  </div>
-                </div>
-                <MyCreateData>{el.createData}</MyCreateData>
-                <div>
-                  <MyImportanceButton {...el}>
-                    {el.importance}
-                  </MyImportanceButton>
-                  <img src={defaultProfile} alt="기본 프로필 이미지" />
-                </div>
-              </MyTask>
-            );
-          })}
-        </div>
-        <div>
-          <MyProgressTitle>
-            <div>
-              <p>🎉</p>
-              <MyTitle fontSize="16px"> Completed</MyTitle>
-            </div>
-            <MyMenuBar src={menu} alt="menu-bar" />
-          </MyProgressTitle>
-          <MyTask>
-            <MyTitle fontSize="13px">➕ Add New Task</MyTitle>
-          </MyTask>
-          {workflowList.done.map(el => {
-            return (
-              <MyTask key={el.id}>
-                <div>
-                  <MyContent>{el.content}</MyContent>
-                  <div>
-                    <span>✏️</span>
-                    <span>❌</span>
-                  </div>
-                </div>
-                <MyCreateData>{el.createData}</MyCreateData>
-                <div>
-                  <MyImportanceButton {...el}>
-                    {el.importance}
-                  </MyImportanceButton>
-                  <img src={defaultProfile} alt="기본 프로필 이미지" />
-                </div>
-              </MyTask>
-            );
-          })}
-        </div>
+        <KanbanProgress
+          workflowList={workflowList.todo}
+          progress="Request"
+          icon={iconList[0]}
+        />
+        <KanbanProgress
+          workflowList={workflowList.inprogress}
+          progress="In Progress"
+          icon={iconList[1]}
+        />
+        <KanbanProgress
+          workflowList={workflowList.inreview}
+          progress="In Review"
+          icon={iconList[2]}
+        />
+        <KanbanProgress
+          workflowList={workflowList.blocked}
+          progress="Blocked"
+          icon={iconList[3]}
+        />
+        <KanbanProgress
+          workflowList={workflowList.done}
+          progress="Completed"
+          icon={iconList[4]}
+        />
       </MyProgressArea>
     </div>
   );
